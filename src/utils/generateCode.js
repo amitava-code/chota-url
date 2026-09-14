abfa130a81637dbdeaac3f87c2c3e2c252d57e5e
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-09-14 20:27:43 +0530

    fix: use base64url encoding for URL-safe short codes

--- src/utils/generateCode.js
+++ src/utils/generateCode.js
@@ -2,7 +2,7 @@ import crypto from 'crypto'
 
 const genetateCode = ()=> {
 
-    let shortCode = (crypto.randomBytes(6).toString('base64').slice(0,6))
+    let shortCode = (crypto.randomBytes(6).toString('base64url').slice(0,6))
 
     return shortCode
 }
